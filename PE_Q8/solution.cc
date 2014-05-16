cb347231744fe5645b5353aa78bc4fb1e5367b0e
Wasiur Rahman <wrahman0@gmail.com>
2014-05-15 23:58:46 -0400

    Finished Q8

--- PE_Q8/solution.cc
+++ PE_Q8/solution.cc
@@ -20,7 +20,21 @@ int main (){
 	}
 
 	//Finding solution
-	
+	int max = arr.at(0) * arr.at(1) * arr.at(2) * arr.at(3) * arr.at(4) ;
+
+
+
+	for (int i = 4; i < arr.size(); i++){
+		
+		int temp = arr.at(i-4) * arr.at(i-3) * arr.at(i-2) * arr.at(i-1) * arr.at(i) ;
+		
+		if (temp > max){
+			max = temp;
+		}
+
+	}
+
+	cout << "MAX:" << max << endl;
 
 	return 0;
 }
