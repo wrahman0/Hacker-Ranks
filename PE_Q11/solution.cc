754d674bfa0aba0e5ddedcf23e3687299d903047
Wasiur Rahman <wrahman0@gmail.com>
2014-05-23 22:32:05 -0400

    more

--- PE_Q11/solution.cc
+++ PE_Q11/solution.cc
@@ -30,21 +30,36 @@ int main (){
 
 	int grid[20][20];
 	int maxProd = -1;
-	int tempProd = -1;
-	bool clear[2];
+	int tempProd = -1;	
 
 	for (int i = 0; i < 20; i++){
+
 		for (int j = 0; j < 20; j++){
-			cin >> grid[i][j];
+			
+			bool *clear = isClear(i,j);
+			
+			if (clear[0]){
+				tempProd = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3];
+				if (tempProd > maxProd)
+					maxProd = tempProd;
+			}
+
+			if (clear[1]){
+				tempProd = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j];
+				if (tempProd > maxProd)
+					maxProd = tempProd;	
+			}
+
+			if (clear[0] && clear[1]){
+				tempProd = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3];
+				if (tempProd > maxProd)
+					maxProd = tempProd;		
+			}
+
 		}
 	}
 
-	for (int i = 0; i < 20; i++){
-		for (int j = 0; j < 20; j++){
-			cout << grid[i][j] << " ";
-		}
-		cout << endl;
-	}
+	cout << maxProd << endl;
 
 	return 0;
 }
