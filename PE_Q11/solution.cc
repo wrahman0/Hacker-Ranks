86b7c05de8aa04282149ab399fb973c8b8932db1
Wasiur Rahman <wrahman0@gmail.com>
2014-05-27 00:11:56 -0400

    Refactored and working

--- PE_Q11/solution.cc
+++ PE_Q11/solution.cc
@@ -26,6 +26,35 @@ bool *isClear (int x, int y){
 
 }
 
+int getMax (int grid[20][20], int i, int j){
+
+	int maxProd = -1;
+	int tempProd = -1;	
+
+	bool *clear = isClear(i,j);
+			
+	if (clear[0]){
+		tempProd = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3];
+		if (tempProd > maxProd)
+			maxProd = tempProd;
+	}
+
+	if (clear[1]){
+		tempProd = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j];
+		if (tempProd > maxProd)
+			maxProd = tempProd;	
+	}
+
+	if (clear[0] && clear[1]){
+		tempProd = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3];
+		if (tempProd > maxProd)
+			maxProd = tempProd;		
+	}
+
+	return maxProd;
+
+}
+
 int main (){
 
 	int grid[20][20];
@@ -39,34 +68,19 @@ int main (){
 		}
 	}
 
+	for (int i = 0; i < 17; i++){
 
-	for (int i = 0; i < 20; i++){
-
-		for (int j = 0; j < 20; j++){
+		for (int j = 0; j < 17; j++){
 			
-			bool *clear = isClear(i,j);
+			tempProd = getMax (grid, i, j);
 			
-			if (clear[0]){
-				tempProd = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3];
-				if (tempProd > maxProd)
-					maxProd = tempProd;
-			}
+			if (tempProd > maxProd)
+				maxProd = tempProd;
 
-			if (clear[1]){
-				tempProd = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j];
-				if (tempProd > maxProd)
-					maxProd = tempProd;	
-			}
-
-			if (clear[0] && clear[1]){
-				tempProd = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3];
-				if (tempProd > maxProd)
-					maxProd = tempProd;		
-			}
-
+			cout << "Max at (" << i << ", " << j << ") is " << getMax (grid,i,j) << endl;
+			
 		}
 	}
-
 	cout << maxProd << endl;
 
 	return 0;
