91f32e214fc1f9dcbbade40219f493b02e412b38
Wasiur Rahman <wrahman0@gmail.com>
2014-05-28 23:30:14 -0400

    Solved
    Takes forever though. There must be a better implementation

--- PE_Q15/solution.cc
+++ PE_Q15/solution.cc
@@ -7,17 +7,29 @@ using namespace std;
 
 int possibleRoutes (int curr_x, int curr_y, int end_x, int end_y){
 
+	cout << " curr_x: " << curr_x
+	<< " curr_y: " << curr_y
+	<< " end_x: " << end_x
+	<< " end_y: " << end_y << endl;
+
 	if (curr_x == end_x && curr_y == end_y){
-		return 0;
+		return 1;
 	}else {
-		if ( curr_x + 1 != end_x )
-			return 1 + possibleRoutes ( curr_x + 1, curr_y, end_x, end_y ) + possibleRoutes ( curr_x, curr_y + 1, end_x, end_y );
+		
+		if ( curr_x + 1 <= end_x && curr_y + 1 <= end_y )
+			return possibleRoutes ( curr_x + 1, curr_y, end_x, end_y ) + possibleRoutes ( curr_x, curr_y + 1, end_x, end_y );
+		else if ( curr_x + 1 <= end_x )
+			return possibleRoutes ( curr_x + 1, curr_y, end_x, end_y );
+		else if ( curr_y + 1 <= end_y )
+			return possibleRoutes ( curr_x, curr_y + 1, end_x, end_y );
+		else return -1000000;
+
 	}
 
 }
 
 int main (){
 
-
+	cout << possibleRoutes (0,0,3,3) << endl;
 	return 0;
 }
